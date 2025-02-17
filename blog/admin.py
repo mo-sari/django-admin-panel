222fb31d91e1c02c01c271bad26fce9ab812a611
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-02-18 02:08:14 +0330

    fieldSet customization, part 2

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,46 @@
-from .models import Post, Category
+from .models import Post
 from django.contrib import admin
-import django.apps
-from django.contrib.contenttypes.models import ContentType
-
-admin.site.register(Category)
+from django import forms
 
 
-class PostAdmin(admin.ModelAdmin):
-    # fields = ['title', ('author', 'slug')]
-    # above we grouped author and slug together
+class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].help_text = "new help text"
 
-    fieldsets = (
-        ('Main Section', {
-            'fields': ('title', 'author'),
-            'description': 'just a simple description',
-        }),
-        ('Section two', {
-            'fields': ('slug',),
-            'classes': ('collapse',),
-        }),
-    )
+    class Meta:
+        model = Post
+        exclude = ('',)
 
 
-admin.site.register(Post, PostAdmin)
+class PostFormAdmin(admin.ModelAdmin):
+    form = PostForm
+
+    # having all the fields like just the line above or
+    # having selective fields with below solution
+
+    # fieldsets = (
+    #     ('First Section', {
+    #         'fields': ('title', 'category'),
+    #         'description': 'just a simple description'
+    #     }),
+    # )
+
+
+admin.site.register(Post, PostFormAdmin)
+
+# ================================================
+# EXTRA POINST
+
+# When to Use a Custom ModelForm in Django Admin?
+# _Add Custom Validation:
+# def clean_title(self):
+#     title = self.cleaned_data['title']
+#     if len(title) < 5:
+#         raise forms.ValidationError("Title must be at least 5 characters long")
+#     return title
+
+# the hierarchy for deciding which fields to be in admin panel is:
+# fieldsets in modelAdmin
+# fields in modelAdmin
+# ModerlForm.Meta.fields
