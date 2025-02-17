61a697e41e1d9db38bcc4f7ef3332cbda2d2dcd6
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-02-17 18:49:12 +0330

    First three commands to adjust titles and headers

--- core/urls.py
+++ core/urls.py
@@ -4,3 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+admin.site.index_title = 'سایت لباس فروشی'
+admin.site.site_header = 'پنل ادمین سایت لباس فروشی'
+admin.site.site_title = 'تایتل سایت'
